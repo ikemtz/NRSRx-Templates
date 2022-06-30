884c00262b20fc7d5f6628200476b00db87d340b
IkeMtz <ikemtz@outlook.com>
2022-06-30 09:09:49 -0400

    Adding VSIX

--- src/IkeMtz.NRSRx.Extensions/NRSRx_ExtensionsPackage.cs
+++ src/IkeMtz.NRSRx.Extensions/NRSRx_ExtensionsPackage.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell;
 using System;
 using System.Runtime.InteropServices;
 using System.Threading;
@@ -30,18 +30,18 @@ namespace NRSRx_Extensions
         /// <summary>
         /// NRSRx_ExtensionsPackage GUID string.
         /// </summary>
-        public const string PackageGuidString = "85b14971-cd38-45bb-b5c9-a21e22dd7d5f";
+        public const string PackageGuidString = "a51319cb-f11e-40be-ba21-12c559fbc060";
 
-        #region Package Members
+    #region Package Members
 
-        /// <summary>
-        /// Initialization of the package; this method is called right after the package is sited, so this is the place
-        /// where you can put all the initialization code that rely on services provided by VisualStudio.
-        /// </summary>
-        /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
-        /// <param name="progress">A provider for progress updates.</param>
-        /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
-        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+    /// <summary>
+    /// Initialization of the package; this method is called right after the package is sited, so this is the place
+    /// where you can put all the initialization code that rely on services provided by VisualStudio.
+    /// </summary>
+    /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
+    /// <param name="progress">A provider for progress updates.</param>
+    /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
+    protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
