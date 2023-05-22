2229dcd9d6acdd8f84dd879872ff2cb2e8dff190
Isaac Martinez (@ikemtz) <ikemtz@outlook.com>
2023-05-22 09:42:33 -0400

    Updating dependencies

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ dotnetVersion=$(dotnet --version)
 export dotnetVersion
 echo "$dotnetVersion"
 
-if [ "$dotnetVersion" == "7.0.100" ]
+if [ "$dotnetVersion" == "7.0.202" ]
 then
   dotnet new install ./src/IkeMtz.NRSRx.Templates/Models --force
   dotnet new install ./src/IkeMtz.NRSRx.Templates/OData --force
